ed801445cd65608ef56101b09cdf75cda721e975
denis96z <zinoviev.denis@yandex.ru>
2018-11-10 21:08:47 +0300

    error responses implemented

--- services/rest/createProfileHandler.go
+++ services/rest/createProfileHandler.go
@@ -7,17 +7,17 @@ import (
 func (srv *Service) createProfile(w http.ResponseWriter, r *http.Request) {
 	var pd NewProfileData
 	if err := srv.readRequestBody(&pd, r); err != nil {
-		srv.writeErrorWithJSON(w, err)
+		srv.writeErrorWithBody(w, err)
 		return
 	}
 
 	if err := pd.Validate(); err != nil {
-		srv.writeErrorWithJSON(w, err)
+		srv.writeErrorWithBody(w, err)
 		return
 	}
 
 	if err := srv.pfRepo.Create(pd.Prepare()); err != nil {
-		srv.writeErrorWithJSON(w, err)
+		srv.writeErrorWithBody(w, err)
 		return
 	}
 
--- services/rest/utils.go
+++ services/rest/utils.go
@@ -40,6 +40,43 @@ func (srv *Service) writeError(w http.ResponseWriter, status int) {
 	w.WriteHeader(status)
 }
 
-func (srv *Service) writeErrorWithJSON(w http.ResponseWriter, err error) {
+func (srv *Service) writeErrorWithBody(w http.ResponseWriter, err error) {
+	switch srvErr := err.(type) {
+	case *errs.NotAuthorizedError:
+		w.WriteHeader(http.StatusUnauthorized)
 
+	case *errs.AccessDeniedError:
+		w.WriteHeader(http.StatusForbidden)
+
+	case *errs.InvalidFormatError:
+		w.WriteHeader(http.StatusUnprocessableEntity)
+
+	case *errs.DuplicateError:
+		w.WriteHeader(http.StatusConflict)
+
+	case *errs.NotFoundError:
+		w.WriteHeader(http.StatusNotFound)
+
+	case *errs.ServiceError:
+		srv.logger.Error(srvErr.InnerError())
+		w.WriteHeader(http.StatusInternalServerError)
+
+	default:
+		srv.logger.Error(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		srv.writeText(w, errs.ServiceErrorMessage)
+		return
+	}
+
+	srv.writeText(w, err.Error())
+}
+
+func (srv *Service) writeText(w http.ResponseWriter, txt string) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte(txt))
+}
+
+func (srv *Service) writeJSON(w http.ResponseWriter, v interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(v)
 }
